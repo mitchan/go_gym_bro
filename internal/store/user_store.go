4dbeafdc9b099cdd6f105be0d790ae1c5541fc96
Francesco Casetta <francesco.casetta@gmail.com>
2025-05-13 07:33:33 +0200

    feat: sql users query

--- internal/store/user_store.go
+++ internal/store/user_store.go
@@ -35,3 +35,73 @@ type UserStore interface {
 	GetUserByUsername(username string) (*User, error)
 	UpdateUser(*User) error
 }
+
+func (s *PostgresUserStore) CreateUser(user *User) error {
+	query := `
+	insert into users (username, email, password_hash, bio)
+	values ($1, $2, $3, $4)
+	returning id, created_at, updated_at`
+
+	err := s.db.QueryRow(query, user.Username, user.Email, user.PasswordHash.hash, user.Bio).Scan(&user.ID, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *PostgresUserStore) GetUserByUsername(username string) (*User, error) {
+	user := &User{
+		PasswordHash: password{},
+	}
+
+	query := `
+	select id, username, email, password_hash, bio, created_at, updated_at
+	from users
+	where username = $1`
+
+	err := s.db.QueryRow(query, username).Scan(
+		&user.ID,
+		&user.Username,
+		&user.Email,
+		&user.PasswordHash.hash,
+		&user.Bio,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (s *PostgresUserStore) UpdateUser(user *User) error {
+	query := `
+	update users
+	set username = $1, email = $2, bio = $4, updated_at = CURRENT_TIMESTAMP
+	where id = $5
+	returning updated_at
+	`
+
+	result, err := s.db.Exec(query, user.Username, user.Email, user.PasswordHash.hash, user.Bio, user.ID)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return sql.ErrNoRows
+	}
+
+	return nil
+}
