4710eb805bcee262eb2e43bde9509f6b2c468335
Francesco Casetta <francesco.casetta@gmail.com>
2025-05-14 08:18:34 +0200

    feat: validate user registration

---
+++ internal/api/user_handler.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"errors"
+	"log"
+	"regexp"
+
+	"github.com/mitchan/go_gym_bro/internal/store"
+)
+
+type registerUserRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Bio      string `json:"bio"`
+}
+
+type UserHandler struct {
+	userStore store.UserStore
+	logger    *log.Logger
+}
+
+func NewUserHandler(userStore store.UserStore, logger *log.Logger) *UserHandler {
+	return &UserHandler{
+		userStore,
+		logger,
+	}
+}
+
+func (h *UserHandler) validateRegisterRequest(req *registerUserRequest) error {
+	if req.Username == "" {
+		return errors.New("username is required")
+	}
+
+	if len(req.Password) < 8 {
+		return errors.New("password length cannot be less than 8 characters")
+	}
+
+	if req.Email == "" {
+		return errors.New("email il required")
+	}
+
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	if !emailRegex.MatchString(req.Email) {
+		return errors.New("invalid email format")
+	}
+
+	return nil
+}
